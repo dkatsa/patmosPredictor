dbdd892c60ca2274a7ed2f1191a8acaa66d81f65
Katsamanis <dimikats1@ee.duth.gr>
2017-04-25 21:26:00 +0200

    debugging

--- predictor1bit.scala
+++ predictor1bit.scala
@@ -90,7 +90,7 @@ class predictor1bit() extends Module {
       PC_BTB_Dec := PC_BTB(io.PC_Fe(PREDICTOR_INDEX_ONE,0))
       targetPC_Reg_Dec := targetPC_Reg(io.PC_Fe(PREDICTOR_INDEX_ONE,0))
       predictor_Dec_Res := predictor(io.PC_Fe(PREDICTOR_INDEX_ONE,0))
-      choose_PC_Dec := (io.choose_PC === UInt(1)) && (! io.doCallRet) && (! io.flush)
+      choose_PC_Dec := (io.choose_PC === UInt(1)) && (! io.memfe.doCallRet) && (! io.flush)
    // Execute
       choose_PC_Ex := (choose_PC_Dec && (! io.flush))
       found_Ex := found_Dec
