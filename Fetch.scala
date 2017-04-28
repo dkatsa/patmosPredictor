8c40ad0e91d9424dc9f92b679481ebea55def7d6
Katsamanis <dimikats1@ee.duth.gr>
2017-04-28 18:14:47 +0200

    reve

--- Fetch.scala
+++ Fetch.scala
@@ -171,12 +171,12 @@ class Fetch(fileName : String) extends Module {
 
   val pc_inc = Mux(pc_next(0), pc_next2, pc_next)
   
-  val pcOdd_Mux = Mux(io.memfe.doCallRet, io.icachefe.relPc.toUInt, pc_cont)
-                  // Mux(io.correct_on_decode_EN, pcOdd_decEx, 
-                  // Mux(io.correct_PC === UInt(1), pcOdd_decEx, pc_cont)))
-  val pcEven_Mux = Mux(io.memfe.doCallRet, io.icachefe.relPc.toUInt + UInt(2), pc_cont2)
-                   // Mux(io.correct_on_decode_EN, pcEven_decEx, 
-                   // Mux(io.correct_PC === UInt(1), pcEven_decEx, pc_cont2)))
+  val pcOdd_Mux = Mux(io.memfe.doCallRet, io.icachefe.relPc.toUInt,// pc_cont)
+                  Mux(io.correct_on_decode_EN, pcOdd_decEx, 
+                  Mux(io.correct_PC === UInt(1), pcOdd_decEx, pc_cont)))
+  val pcEven_Mux = Mux(io.memfe.doCallRet, io.icachefe.relPc.toUInt + UInt(2), //pc_cont2)
+                   Mux(io.correct_on_decode_EN, pcEven_decEx, 
+                   Mux(io.correct_PC === UInt(1), pcEven_decEx, pc_cont2)))
   
   addrEven := addrEvenReg
   addrOdd := addrOddReg
